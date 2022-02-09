11d68e32422be83cd17f7e3a26c71cc77b9a96ea
greatgaetan <gaetandidderen@gmail.com>
2022-02-09 09:27:53 +0100

    need to update node on computer at home

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -1293,7 +1293,7 @@ querySelector("body");
 toggleMenu.addEventListener("click", function () {
   body.classList.toggle("open");
 });
-},{"swup":"node_modules/swup/lib/index.js"}],"C:/Users/Gaëtan/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"swup":"node_modules/swup/lib/index.js"}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -1321,7 +1321,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50347" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54493" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -1497,5 +1497,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["C:/Users/Gaëtan/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
+},{}]},{},["../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
 //# sourceMappingURL=/js.00a46daa.js.map
