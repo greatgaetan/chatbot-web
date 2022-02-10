4adcd4930888933e46564713d9b196b0632bcda4
Gaëtan Didderen <g.didderen@student.helmo.be>
2022-02-10 11:49:54 +0100

    added responsive menu on all pages

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -1306,7 +1306,7 @@ for (var i = 0; i < aTags.length; i++) {
 toggleMenu.addEventListener("click", function () {
   body.classList.toggle("open");
 });
-},{"swup":"node_modules/swup/lib/index.js"}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"swup":"node_modules/swup/lib/index.js"}],"C:/Users/Gaëtan/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -1334,7 +1334,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60358" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54702" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -1510,5 +1510,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
+},{}]},{},["C:/Users/Gaëtan/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
 //# sourceMappingURL=/js.00a46daa.js.map
