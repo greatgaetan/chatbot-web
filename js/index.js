29e0cd6142dcf27068ab11e8242c3ab847e94120
Gaëtan Didderen <g.didderen@student.helmo.be>
2022-02-10 13:51:13 +0100

    remove js comments

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
-import Swup from "../node_modules/swup/dist/swup";
-const swup = new Swup(); // only this line when included with script tag
-
+import Swup from "swup";
+const swup = new Swup();
 var toggleMenu = document.querySelector(".toggleMenu");
 var body = document.querySelector("body");
 
-/**
- * Getting "a" tags from navbar to add click event to
- * close menu on mobile view.
- */
 var navbar = document.getElementById("navbar");
 var aTags = navbar.getElementsByTagName("a");
 for (var i = 0; i < aTags.length; i++) {
